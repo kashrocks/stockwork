d45b19a1994c9c4ed3261b361060883cd7559f7d
kashrocks <kashyap.achar@gmail.com>
2019-07-17 16:22:46 -0400

    New changes

--- web_scrape.py
+++ web_scrape.py
@@ -1,30 +1,42 @@
 import requests
 import json
 import datetime
+import time
 
 API_URL = "https://www.alphavantage.co/query"
 API_KEY = "CU6GB0TZL1ZURLJY"
-TIME_INTERVAL = "15min"
+TIME_INTERVAL_TIME = 1
+TIME_INTERVAL = str(TIME_INTERVAL_TIME) + "min"
 FUNCS = ["GLOBAL_QUOTE", "TIME_SERIES_INTRADAY"]
-TICKERS = ["MTLO", "SNAP", "BYND"]
+TICKERS = ["GOOGL", "SNAP", "BYND"]
 
 
 def get_prcnt_change(ticker, data):
     
     # getting current time and date in certain format
     c_time = datetime.datetime.now()
-    # c_time = c_time - datetime.timedelta(minutes=c_time.minute % 15)
+
+    # c_time = c_time - datetime.timedelta(minutes=c_time.minute % TIME_INTERVAL_TIME)
     # c_time = c_time.replace(second=0)
-    # replace when not testing
-    c_time = c_time.replace(hour=14, minute=15, second=0)
+
+    # replace when not testing #########################
+    c_time = c_time.replace(hour=16, minute=0, second=0)
+
+    # dont change the below
     c_time_text = c_time.strftime("%Y-%m-%d %H:%M:%S")
 
-    # getting current time and date in certain format - 15 mins
-    l_time = c_time - datetime.timedelta(minutes=15)
+    # getting current time and date in certain format - TIME_INTERVAL_TIME mins
+    l_time = c_time - datetime.timedelta(minutes=TIME_INTERVAL_TIME)
     l_time_text = l_time.strftime("%Y-%m-%d %H:%M:%S")
 
     # getting current price and last price
-    rec_price = data["Time Series (" + TIME_INTERVAL + ")"][c_time_text]["4. close"]
+    all_keys = list(data["Time Series (" + TIME_INTERVAL + ")"].keys())
+    if c_time_text not in all_keys:
+        print("Current time: " + all_keys[-1])
+        rec_price = data["Time Series (" + TIME_INTERVAL + ")"][all_keys[-1]]["4. close"]
+    else:
+        print("Current time: " + c_time_text)
+        rec_price = data["Time Series (" + TIME_INTERVAL + ")"][c_time_text]["4. close"]
     last_price = data["Time Series (" + TIME_INTERVAL + ")"][l_time_text]["4. close"]
 
     print('This is rec_price: ' + rec_price)
@@ -71,5 +83,10 @@ def data_file(data=None):
         return
 
 
-get_stock_data()
-# for_testing()
+# for min in range(5):
+#     get_stock_data()
+#     for_testing()
+#     time.sleep(61)
+
+#get_stock_data()
+for_testing()
